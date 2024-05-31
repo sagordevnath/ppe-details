1a6504641b45a0b03a99e174dfcbd7d0926334b7
sagordevnsth <ssagor64@gmail.com>
2024-05-31 13:47:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Introduction from './Pages/Introduction'
+import Menubar from './Pages/MenuBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+    <Menubar />
+    </>
+  )
+}
+
+export default App
